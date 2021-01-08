34aa2b4bec56643946efa4f889a387506f0a229d
Biljana Zivkovic <77129646+biljanazivkovic@users.noreply.github.com>
2021-01-08 16:19:52 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex11
+{   //The class
+    class Car
+    {
+        //Class members
+        string color = "red";
+        int maxSpeed;
+        public void fullThrottle()
+        {
+            Console.WriteLine("The car is going as fast as it can!");
+        }
+
+        static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            myObj.maxSpeed = 200;
+            Console.WriteLine(myObj.color);
+            Console.WriteLine(myObj.maxSpeed);
+            myObj.fullThrottle();
+        }
+    }
+}
